34e4152d9af6bde0f7ba2d45e94be9fad0652a51
ImGabreuw <imgabreuw.dev@gmail.com>
2022-03-16 18:46:20 -0300

    feat: registry Controller router

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,41 @@
+import config from './config.js';
+import { Controller } from './controller.js';
+import { logger } from './utils.js';
+
+const {
+  location,
+  pages: {
+    homeHTML
+  }
+} = config;
+
+const controller = new Controller();
+
+async function routes(request, response) {
+  const { method, url } = request;
+
+  if (method === 'GET' && url === '/') {
+    response.writeHead(302, {
+      'Location': location.home
+    });
+
+    return response.end();
+  }
+
+  if (method === 'GET' && url === '/home') {
+    const { stream, type } = await controller.getFileStream(homeHTML);
+
+    response.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+
+    return stream.pipe(response);
+  }
+
+  return response.end('hello')
+}
+
 export function handler(request, response) {
-  return response.end('hello');
+  return routes(request, response)
+    .catch(error => logger.error(`Erro: ${error.stack}`))
 }
