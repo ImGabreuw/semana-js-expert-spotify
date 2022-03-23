31fedac4597e941ccb67b492b85143a1a6de92cf
ImGabreuw <imgabreuw.dev@gmail.com>
2022-03-23 20:51:27 -0300

    fix: asset not found

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ import config from './config.js';
 import server from './server.js';
 import { logger } from './utils.js';
 
-server
+// fix: como agora é uma função mudei a chamada
+server()
   .listen(config.port)
-  .on('listening', () => logger.info(`server listening on port ${config.port}!`));
+  .on('listening', () => {
+    logger.info(`server running at ${config.port}!!`)
+  });
+
+process.on('uncaughtException', (error) => logger.error(`unhandledRejection happened: ${error.stack || error }`));
+process.on('unhandledRejection', (error) => logger.error(`unhandledRejection happened: ${error.stack || error }`));
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 import { createServer } from "http";
-
+import config from './config.js';
 import { handler } from './routes.js'
 
 export default () => createServer(handler)
